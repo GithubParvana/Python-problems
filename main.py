4fee712cf11e7e2136b17a9f122e7bc3ca354751
parvana <pyva0055@gmail.com>
2025-01-12 21:51:51 +0400

    added code

---
+++ main.py
@@ -0,0 +1,18 @@
+import math
+
+a = int(input())
+b = int(input())
+
+
+def square_numbers():
+    if a >= 1 and b <= 2025:
+        output = (a + b) ** 2
+    else:
+        output = "a or b does not meet requirements"
+    return output
+
+
+
+print(square_numbers())
+
+
